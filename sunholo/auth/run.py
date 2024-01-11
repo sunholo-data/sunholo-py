b0ddb90d91d17abaa1deb978335bcc770e4668c6
Mark <github@markedmondson.me>
2024-01-11 15:29:09 +0100

    more auth logging

--- sunholo/auth/run.py
+++ sunholo/auth/run.py
@@ -1,5 +1,6 @@
 # from https://github.com/sunholo-data/genai-databases-retrieval-app/blob/main/langchain_tools_demo/agent.py
 import os
+import inspect
 import google.auth.transport.requests  # type: ignore
 import google.oauth2.id_token  # type: ignore
 from typing import Dict, Optional
@@ -33,6 +34,7 @@ def get_id_token(url: str) -> str:
     # Use Application Default Credentials on Cloud Run
     if is_running_on_cloudrun():
         auth_req = google.auth.transport.requests.Request()
+        logging.info(f'Got id_token for {url}')
         return google.oauth2.id_token.fetch_id_token(auth_req, url)
     else:
         # Use gcloud credentials locally
@@ -53,9 +55,25 @@ def get_header() -> Optional[dict]:
         run_url = get_run_url()
     else:
         run_url = "127.0.0.1:8080"
+
     if "http://" in run_url:
         return None
     else:
         # Append ID Token to make authenticated requests to Cloud Run services
+        frame = inspect.currentframe()
+        caller_frame = frame.f_back if frame is not None else None  # One level up in the stack
+        deets = {
+            'message': 'Authenticating for run_url',
+            'run_url': run_url
+        }
+        if caller_frame:
+            deets = {
+                    'message': 'Authenticating for run_url',
+                    'file': caller_frame.f_code.co_filename,
+                    'line': str(caller_frame.f_lineno),  
+                    'function': caller_frame.f_code.co_name,
+                    'run_url': run_url
+                }
+        logging.info(log_struct=deets)
         headers = {"Authorization": f"Bearer {get_id_token(run_url)}"}
         return headers
