2045f9921dd1fd47747f1903a3932ab7beca91bc
Mark <github@markedmondson.me>
2024-03-21 17:42:43 +0100

    imports

--- sunholo/chunker/pdfs.py
+++ sunholo/chunker/pdfs.py
@@ -11,10 +11,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import sys, os
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(parent_dir)
-
+import os
 import pathlib
 from ..logging import setup_logging
 
