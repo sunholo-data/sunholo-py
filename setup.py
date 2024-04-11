0341da0a4d4faa39adb892b4262f31e8fb5bfa8a
Mark <github@markedmondson.me>
2024-04-11 18:38:21 +0200

    add langfuse tracking

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Define your base version
-version = '0.42.0'
+version = '0.42.1'
 
 setup(
     name='sunholo',
--- sunholo/langfuse/callback.py
+++ sunholo/langfuse/callback.py
@@ -1,4 +1,7 @@
 from langfuse.callback import CallbackHandler
+from fastapi import Request
+from typing import Dict, Any
+
 
 def create_langfuse_callback():
     langfuse_handler = CallbackHandler()
@@ -7,3 +10,32 @@ def create_langfuse_callback():
     langfuse_handler.auth_check()
 
     return langfuse_handler
+
+def add_langfuse_tracing(
+        config: Dict[str, Any],
+        request: Request) -> Dict[str, Any]:
+    """
+    Config modifier function to add a tracing callback
+
+    :param config: config dict
+    :param request: HTTP request
+    :return: updated config
+    """
+
+    if "callbacks" not in config:
+        config["callbacks"] = []
+
+    langfuse_handler = CallbackHandler(
+        user_id = config.get("user_id"),
+        session_id = config.get("session_id"),
+    )
+
+    config["callbacks"].extend([langfuse_handler])
+
+    return config
+
+
+#add_routes(app, my_chain,
+#           path="/my-chain",
+#           per_req_config_modifier=_add_tracing,
+#           config_keys=["configurable", "session_id", "user_id"])
