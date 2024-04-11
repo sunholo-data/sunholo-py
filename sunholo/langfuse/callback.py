bbf2774011724b563ab82fa695248e7b275df7de
Mark <github@markedmondson.me>
2024-04-11 18:43:30 +0200

    use create_langfuse

--- sunholo/langfuse/callback.py
+++ sunholo/langfuse/callback.py
@@ -3,8 +3,9 @@ from fastapi import Request
 from typing import Dict, Any
 
 
-def create_langfuse_callback():
-    langfuse_handler = CallbackHandler()
+def create_langfuse_callback(**kwargs):
+
+    langfuse_handler = CallbackHandler(**kwargs)
 
     # Tests the SDK connection with the server
     langfuse_handler.auth_check()
@@ -25,11 +26,10 @@ def add_langfuse_tracing(
     if "callbacks" not in config:
         config["callbacks"] = []
 
-    langfuse_handler = CallbackHandler(
+    langfuse_handler = create_langfuse_callback(
         user_id = config.get("user_id"),
-        session_id = config.get("session_id"),
+        session_id = config.get("session_id")
     )
-
     config["callbacks"].extend([langfuse_handler])
 
     return config
