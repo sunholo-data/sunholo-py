6d5edd5ed0fabd0a3e4ccaf26b0c1d9b5c4a8dd0
Mark <github@markedmondson.me>
2024-04-30 14:40:22 +0200

    allow no GCS metadata fetch

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Define your base version
-version = '0.47.0'
+version = '0.48.0'
 
 setup(
     name='sunholo',
--- sunholo/gcs/metadata.py
+++ sunholo/gcs/metadata.py
@@ -8,7 +8,11 @@ def get_object_metadata(bucket_name, object_name):
     if bucket_name is None or object_name is None:
         log.warning("Got invalid bucket name and object name")
         return None
-    storage_client = storage.Client()
+    try:
+        storage_client = storage.Client()
+    except Exception as e:
+        log.warning(f"Could not connect to Google Cloud Storage for metadata: {str(e)}")
+        return None
 
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(object_name)
