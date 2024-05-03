3862289b992f1ded74688bffa3ed01ef26c99e9f
Mark <github@markedmondson.me>
2024-05-03 17:37:35 +0200

    duplicates

--- build_scripts/export_docstrings.py
+++ build_scripts/export_docstrings.py
@@ -15,14 +15,18 @@ def list_classes(module):
 def list_all_functions_and_classes_in_package(package):
     functions = []
     classes = []
-    for _, module in inspect.getmembers(package, inspect.ismodule):
+    visited_modules = set()
+
+    def explore(module):
+        if module in visited_modules:
+            return
+        visited_modules.add(module)
         functions.extend(list_functions(module))
         classes.extend(list_classes(module))
-        # Recursively find submodules
-        submodules = inspect.getmembers(module, inspect.ismodule)
-        for _, submodule in submodules:
-            functions.extend(list_functions(submodule))
-            classes.extend(list_classes(submodule))
+        for _, submodule in inspect.getmembers(module, inspect.ismodule):
+            explore(submodule)
+
+    explore(package)
     return functions, classes
 
 def append_docstrings_to_md(package, output_file='docs/docs/functions.md'):
@@ -35,21 +39,21 @@ def append_docstrings_to_md(package, output_file='docs/docs/functions.md'):
         print(f"Found classes: {[cls_name for cls_name, _, _ in classes]}")
 
     with open(output_file, 'a') as f:
-        f.write("# Functions\n\n")
+        f.write("## Functions\n\n")
         for func_name, func, signature, source_file in functions:
             relative_file_path = os.path.relpath(source_file)
             docstring = inspect.getdoc(func)
-            f.write(f"## {func_name}{signature}\n")
+            f.write(f"### {func_name}{signature}\n")
             f.write(f"*Source*: [{relative_file_path}]({GITHUB_BASE_URL + relative_file_path})\n")
-            f.write(f"{docstring or 'No docstring available.'}\n\n")
+            f.write(f"\n{docstring or 'No docstring available.'}\n\n")
 
-        f.write("# Classes\n\n")
+        f.write("## Classes\n\n")
         for cls_name, cls, source_file in classes:
             relative_file_path = os.path.relpath(source_file)
             cls_docstring = inspect.getdoc(cls)
-            f.write(f"## {cls_name}\n")
+            f.write(f"### {cls_name}\n")
             f.write(f"*Source*: [{relative_file_path}]({GITHUB_BASE_URL + relative_file_path})\n")
-            f.write(f"{cls_docstring or 'No docstring available.'}\n\n")
+            f.write(f"\n{cls_docstring or 'No docstring available.'}\n\n")
             for method_name, method in inspect.getmembers(cls, inspect.isfunction):
                 signature = inspect.signature(method)
                 method_docstring = inspect.getdoc(method)
