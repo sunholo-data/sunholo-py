8147624a121595ca4139fe3d170230620cc8fc50
Mark <github@markedmondson.me>
2024-05-29 12:00:05 +0200

    import errs

--- sunholo/database/lancedb.py
+++ sunholo/database/lancedb.py
@@ -1,15 +1,20 @@
-import lancedb
-import tantivy
+from ..logging import log
 
 def create_lancedb_index(bucket, vector_name, num_partitions=256, num_sub_vectors=96, text_key="text"):
+    try:
+        import lancedb
+        #import tantivy
+    except ImportError:
+        raise ValueError("Could not import lancedb module, install via `pip intall sunholo[database]`")
+        
     try:
         db = lancedb.connect(bucket) 
         tbl = db.open_table(vector_name)
 
         tbl.create_index(num_partitions=num_partitions, num_sub_vectors=num_sub_vectors)
         tbl.create_fts_index(text_key)
-        print(f'Index creation for {vector_name} success')
+        log.info(f'Index creation for {vector_name} success')
     except Exception as e: 
-        print(f'Index creation for {vector_name} failed: {e}')
+        log.info(f'Index creation for {vector_name} failed: {e}')
     
     
