00f380f32cc399454fabdff8ec795870a7527328
Mark <github@markedmondson.me>
2024-06-10 12:20:39 +0200

    easy safety

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Define your base version
-version = '0.62.13'
+version = '0.62.14'
 
 setup(
     name='sunholo',
--- sunholo/vertex/__init__.py
+++ sunholo/vertex/__init__.py
@@ -1,2 +1,3 @@
 from .init import init_vertex
 from .memory_tools import get_vertex_memories, print_grounding_response
+from .safety import vertex_safety
