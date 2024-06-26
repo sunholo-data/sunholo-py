3f70b9d0486a9571e6689c1556ccc35c9365120d
Mark <github@markedmondson.me>
2024-06-26 11:46:10 +0200

    openai

--- sunholo/gcs/add_file.py
+++ sunholo/gcs/add_file.py
@@ -26,8 +26,8 @@ from ..utils.config import load_config_key
 
 
 def handle_base64_image(base64_data, vector_name):
-    model = load_config_key("model", vector_name, "vacConfig")
-    if model.startswith("gpt"): # pass it to gpt directly
+    model = load_config_key("llm", vector_name, "vacConfig")
+    if model.startswith("openai"): # pass it to gpt directly
         return base64_data, base64_data.split(",",1)
     
     try:
