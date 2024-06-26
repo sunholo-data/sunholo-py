1c788158a9dfd005aa5ab8cd3cdc93ee52d5cf81
Mark <github@markedmondson.me>
2024-06-26 20:27:22 +0200

    import docs

--- sunholo/database/discovery_engine.py
+++ sunholo/database/discovery_engine.py
@@ -6,6 +6,7 @@ except ImportError:
     discoveryengine = None
 
 from ..logging import log
+from typing import Optional
 
 class DiscoveryEngineClient:
     """
@@ -19,7 +20,7 @@ class DiscoveryEngineClient:
     Example:
         ```python
         client = DiscoveryEngineClient(project_id='your-project-id', data_store_id='your-data-store-id')
-        
+
         # Create a collection
         collection_name = client.create_collection("my_new_collection")
 
@@ -61,38 +62,14 @@ class DiscoveryEngineClient:
             else None
         )
         self.client = discoveryengine.DataStoreServiceClient(client_options=client_options)
+        self.parent = self.client.branch_path(
+            project=project_id,
+            location=location,
+            data_store=data_store_id,
+            branch="default_branch",
+        )
+
         
-
-    def create_collection(self, collection_id: str) -> str:
-        """
-        Creates a new collection within the specified data store.
-
-        Args:
-            collection_id (str): The ID of the collection to create.
-
-        Returns:
-            str: The resource name of the created collection.
-
-        Example:
-            ```python
-            collection_name = client.create_collection('my_new_collection')
-            `
-        """
-
-        parent = self.client.data_store_path(
-            project=self.project_id, location=self.location, data_store=self.data_store_id
-        )
-
-        collection = discoveryengine.Collection(display_name=collection_id)
-        request = discoveryengine.CreateCollectionRequest(
-            parent=parent, collection_id=collection_id, collection=collection
-        )
-
-        operation = self.client.create_collection(request=request)
-        log.info(f"Waiting for operation to complete: {operation.operation.name}")
-        response = operation.result()
-
-        return response.name
 
     def create_data_store(
         self, chunk_size: int = 500
@@ -106,7 +83,6 @@ class DiscoveryEngineClient:
         Returns:
             str: The name of the long-running operation for data store creation.
         """
-        parent = self.client.common_location_path(project=self.project_id, location=self.location)
 
         # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1alpha.types.DocumentProcessingConfig
         doc_config = discoveryengine.DocumentProcessingConfig(
@@ -137,7 +113,7 @@ class DiscoveryEngineClient:
 
         # https://cloud.google.com/python/docs/reference/discoveryengine/0.11.4/google.cloud.discoveryengine_v1alpha.types.CreateDataStoreRequest
         request = discoveryengine.CreateDataStoreRequest(
-            parent=parent,
+            parent=self.parent,
             data_store_id=self.data_store_id,
             data_store=data_store,
             # Optional: For Advanced Site Search Only
@@ -162,7 +138,6 @@ class DiscoveryEngineClient:
     def get_chunks(
         self,
         query: str,
-        collection_id: str,
         num_previous_chunks: int = 3,
         num_next_chunks: int = 3,
         page_size: int = 10,
@@ -196,13 +171,10 @@ class DiscoveryEngineClient:
                 serving_config="default_serving_config")
                 ).name
         
-        filter = f'content_search=true AND collection_id="{collection_id}"'
-
         search_request = discoveryengine.SearchRequest(
             serving_config=serving_config,
             query=query,
             page_size=page_size, 
-            filter=filter,
             content_search_spec=discoveryengine.SearchRequest.ContentSearchSpec(
                 #snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
                 #    return_snippet=True
@@ -218,4 +190,56 @@ class DiscoveryEngineClient:
         search_response = self.client.search(search_request)
 
         return search_response
+
+    def import_documents(self,
+        gcs_uri: Optional[str] = None,
+        data_schema="content",
+        bigquery_dataset: Optional[str] = None,
+        bigquery_table: Optional[str] = None,
+        bigquery_project_id: Optional[str] = None,
+    ) -> str:
+        """
+        Args:
+        - gcs_uri: Required. Cloud Storage URIs to input files. Each URI can be up to 2000 characters long. URIs can match the full object path (for example, gs://bucket/directory/object.json) or a pattern matching one or more files, such as gs://bucket/directory/*.json. A request can contain at most 100 files (or 100,000 files if data_schema is content). Each file can be up to 2 GB (or 100 MB if data_schema is content). 
+        - data_schema: Must be one of 'user_event', 'custom' or 'document' if using BigQuery. Default 'content' only for GCS. The schema to use when parsing the data from the source. Supported values for document imports: - document (default): One JSON Document per line. Each document must have a valid Document.id. - content: Unstructured data (e.g. PDF, HTML). Each file matched by input_uris becomes a document, with the ID set to the first 128 bits of SHA256(URI) encoded as a hex string. - custom: One custom data JSON per row in arbitrary format that conforms to the defined Schema of the data store. This can only be used by the GENERIC Data Store vertical. - csv: A CSV file with header conforming to the defined Schema of the data store. Each entry after the header is imported as a Document. This can only be used by the GENERIC Data Store vertical. Supported values for user event imports: - user_event (default): One JSON UserEvent per line. 
+        
+        """
+
+        if gcs_uri:
+            request = discoveryengine.ImportDocumentsRequest(
+                parent=self.parent,
+                # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1alpha.types.GcsSource
+                gcs_source=discoveryengine.GcsSource(
+                    input_uris=[gcs_uri], data_schema=data_schema,
+                ),
+                # Options: `FULL`, `INCREMENTAL`
+                reconciliation_mode=discoveryengine.ImportDocumentsRequest.ReconciliationMode.INCREMENTAL,
+            )
+        else:
+            request = discoveryengine.ImportDocumentsRequest(
+                parent=self.parent,
+                bigquery_source=discoveryengine.BigQuerySource(
+                    project_id=bigquery_project_id or self.project_id,
+                    dataset_id=bigquery_dataset,
+                    table_id=bigquery_table,
+                    data_schema=data_schema,
+                ),
+                # Options: `FULL`, `INCREMENTAL`
+                reconciliation_mode=discoveryengine.ImportDocumentsRequest.ReconciliationMode.INCREMENTAL,
+            )
+
+        # Make the request
+        operation = self.client.import_documents(request=request)
+
+        log.info(f"Waiting for operation to complete: {operation.operation.name}")
+        response = operation.result()
+
+        # Once the operation is complete,
+        # get information from operation metadata
+        metadata = discoveryengine.ImportDocumentsMetadata(operation.metadata)
+
+        # Handle the response
+        log.info(f"{response=} {metadata=}")
+
+        return operation.operation.name
 
