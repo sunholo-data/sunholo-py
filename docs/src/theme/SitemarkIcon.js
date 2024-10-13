074705a01345e23b88e3843e0374e471033ee962
Mark <github@markedmondson.me>
2024-10-13 07:45:55 +0200

    fix footer logo

--- docs/src/theme/SitemarkIcon.js
+++ docs/src/theme/SitemarkIcon.js
@@ -4,7 +4,7 @@ import SvgIcon from '@mui/material/SvgIcon';
 export default function SitemarkIcon() {
   return (
     <img
-    src="img/logo-warm-colors-final.png"
+    src="/img/logo-warm-colors-final.png"  // Use an absolute path
     alt="Sunholo Multivac"
     style={{ width: '120px', height: 'auto', marginRight: '8px', marginBottom: '11px' }}
     />
