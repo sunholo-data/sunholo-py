f6b47c1fc2c8a0fca76a27f65981ff8aba94a574
Mark <github@markedmondson.me>
2024-12-04 11:28:12 +0100

    this is how it should work with chat objects

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.112.6'
+version = '0.113.0'
 
 setup(
     name='sunholo',
--- sunholo/genai/process_funcs_cls.py
+++ sunholo/genai/process_funcs_cls.py
@@ -620,7 +620,9 @@ class GenAIFunctionProcessor:
                 this_text += token
 
             if this_text:
-                content.append(f"Agent: {this_text}")    
+                #content.append(f"Agent: {this_text}")    
+                # update content relying on gemini chat history instead
+                content = [f"Agent:\n{this_text}"]
                 # if text includes gs:// try to download it
                 image_uploads = extract_gs_images_and_genai_upload(this_text)
                 if image_uploads:
@@ -632,7 +634,7 @@ class GenAIFunctionProcessor:
                 big_result.append(this_text)
             else:
                 log.warning(f"[{guardrail}] No content created this loop")
-                content.append(f"Agent: No response was found for loop [{guardrail}]")
+                content = [f"Agent: ERROR - No response was found for loop [{guardrail}]"]
 
             token_queue.append(f"\n----Loop [{guardrail}] End------\n{usage_metadata}\n----------------------")
             loop_span.end(output=content, metadata=loop_metadata) if loop_span else None
