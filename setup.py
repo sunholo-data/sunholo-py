abfe85eb6d1775e6fcfa6ea62879f4882413013f
Mark <github@markedmondson.me>
2024-12-09 11:47:45 +0100

    aiohttp

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     },
     install_requires=[
         # Base dependencies
+        "aiohttp",
         "google-auth", # to check if on gcp
         "requests",
         "ruamel.yaml",
@@ -34,6 +35,7 @@ setup(
     extras_require={
         # Define optional dependencies with feature names
         'all': [
+            "aiohttp",
             "anthropic[vertex]",
             "asyncpg",
             "azure-identity",
