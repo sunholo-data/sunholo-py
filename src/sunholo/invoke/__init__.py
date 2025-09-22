1cdda72f999b0d85bb793e0abd15da4e0ceea7a6
Mark <github@markedmondson.me>
2025-09-22 13:16:06 +0200

    bug importing old AsyncTaskRunner

--- src/sunholo/invoke/__init__.py
+++ src/sunholo/invoke/__init__.py
@@ -1,3 +1,3 @@
 from .invoke_vac_utils import invoke_vac
 from .direct_vac_func import direct_vac, direct_vac_stream, async_direct_vac, async_direct_vac_stream
-from .async_class import AsyncTaskRunner
+from .async_task_runner import AsyncTaskRunner, CallbackContext, TaskConfig
